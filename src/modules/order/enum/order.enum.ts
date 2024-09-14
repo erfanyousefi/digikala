c04dcb7d9df228df09b6f360bf41b667dede00b6
Erfan Yousefi <erfanyousefi.co@gmail.com>
2024-09-15 00:23:43 +0330

    create order enum

---
+++ src/modules/order/enum/order.enum.ts
@@ -0,0 +1,9 @@
+export enum OrderStatus {
+    Pending = "pending",
+    Ordered="ordered",
+    InProcess="in-process",
+    Packed="Packed",
+    InTransit="in-transit",
+    Canceled="canceled",
+    Delivered="delivered"
+}
